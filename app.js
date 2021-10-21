8fcb6d6719e578eccbf97c63c0aaaf46865d76a2
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2021-10-22 00:00:00 +0100

    feature: added morgan and nodeman package

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ const router = express.Router();
 const contactRoutes = require("./api/routes/contacts");
 const todoRoutes = require("./api/routes/todos");
 const usertRoutes = require("./api/routes/users");
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
 
 //Routes
 app.use("/contacts", contactRoutes);
 app.use("/todos", todoRoutes);
 app.use("/users", usertRoutes);
+
+//Error Handling
+app.use((req, res, next) => {
+  const error = new Error("Route not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: { message: error.message },
+  });
+});
 
 //app.use(router);
 
